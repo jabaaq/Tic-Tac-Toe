9256c78008aedde4cdde785d71d32141259374d1
Jaba <jabaqadagishvili4@gmail.com>
2023-09-03 09:34:23 +0400

    Game board added

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const GameBoard = (() => {
+    let board = document.querySelector('.board-grid')
+    let boardArr = ['', '', '', '', '', '', '', '', ''];
+
+    const render = () => {
+        let boardHTML = '';
+        boardArr.forEach((square, index) => {
+
+            boardHTML += `<div class="cell" id='square-${index}'>${square}</div>`
+        })
+        board.innerHTML = boardHTML;
+    }
+
+
+    return {
+        render,
+    }
+})();
+
+
+const createPlayers = ((name, mark) => {
+
+})();
+document.querySelector('#charX').addEventListener('click', GameBoard.render)
+
+
